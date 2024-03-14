28d2b7bbc4eeb32c0e2efd8be1d8dd8816e649e0
Some0ne3lse <143652507+Some0ne3lse@users.noreply.github.com>
2024-03-14 23:33:22 +0000

    Make boom addition instead of multiplication

--- scripts-old.js
+++ scripts-old.js
@@ -1,37 +1,43 @@
-let number = 0;
+let number = 1;
 
 const numberTag = document.getElementById('number');
 
 const list = document.getElementById('boom-list');
 
+let clickNumber = 0;
+
 const isEmpty = (id) => {
   return document.getElementById(id).innerHTML.trim() == ''
 }
 
+
 const onIncrementClick = () => {
-  number = number + 5;
+  number = number * 5;
   numberTag.textContent = number;
-  if (number === 35 && isEmpty('boom-list')) {
+  ++clickNumber;
+  if ((clickNumber === 35 || clickNumber === -35) && isEmpty('boom-list') === true) {
     const boomItem = document.createElement('li');
     boomItem.textContent = '!bOoMm!';
     list.appendChild(boomItem);
-  } else if (number < 35 && number > -35) {
+  } else if (clickNumber < 35 && clickNumber > -35) {
     while (list.firstChild) {
       list.removeChild(list.firstChild);
     }
   }
 }
 
+
 const onDecrementClick = () => {
-  number = number - 5;
+  number = number / 5;
   numberTag.textContent = number;
-  if (number === -35 && isEmpty('boom-list')) {
+  --clickNumber;
+  if ((clickNumber === 35 || clickNumber === -35) && isEmpty('boom-list') === true) {
     const boomItem = document.createElement('li');
     boomItem.textContent = '!bOoMm!';
     list.appendChild(boomItem);
-  } else if (number < 35 && number > -35) {
+  } else if (clickNumber < 35 && clickNumber > -35) {
     while (list.firstChild) {
-      list.removeChild(list.firstChild);
+      list.removeChild(list.firstChild)
     }
   }
 }
